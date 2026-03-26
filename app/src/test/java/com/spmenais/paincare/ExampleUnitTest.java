66fb5b943da6eba049d7e40c21321070d29a5838
oumaimaLahkiar <oumaima_lahkiar@um5.ac.ma>
2026-03-26 14:15:36 +0100

    ci: add JaCoCo and unit tests for coverage

---
+++ app/src/test/java/com/spmenais/paincare/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.spmenais.paincare;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testAlwaysPass() {
+        assertTrue(true);
+    }
+}
